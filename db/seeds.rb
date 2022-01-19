f4e7a61de84bd861fa44da92e5246a3744e05d60
Vasily Toropchin <vasview@gmail.com>
2022-01-20 00:10:09 +0600

    Added City model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CITIES = ['London', 'Moscow', 'Bishkek', 'Almata', 'Osh', 'Berlin', 'Paris']
+
+if City.all.count.zero?
+  CITIES.each do |city|
+    City.create(name: city)
+  end
+end
