538331d0baf9e5491a3c9e7941ba8bbcf816a47c
Vasily Toropchin <vasview@gmail.com>
2022-01-20 12:17:39 +0600

    Added translation for enums

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,6 +1,6 @@
 class RealEstate < ApplicationRecord
-  enum real_estate_type: [:appartment, :house, :office, :other]
-  enum wall_material: [:brick, :concrete, :wood, :other_material]
+  enum :real_estate_type, [:appartment, :house, :office, :other]
+  enum :wall_material, [:brick, :concrete, :wood, :other_material]
 
   belongs_to :city, optional: true
   has_one_attached :header_image
